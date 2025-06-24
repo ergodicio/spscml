9916021b9ae13184c538003d46970c62ba9e97d9
pegahamiri <pamiri@cougarnet.uh.edu>
2025-06-24 16:06:36 -0700

    Update solver.py

--- src/spscml/straightforward_dlra/solver.py
+++ src/spscml/straightforward_dlra/solver.py
@@ -433,9 +433,9 @@ class Solver(eqx.Module):
         L_bcs = jnp.pad(L,[(0,0),(2,2)], mode='empty')
         E_flux_func = lambda left, right: E_plus_matrix @ left + E_minus_matrix @ right
         E_flux = slope_limited_flux(L_bcs, SCHEME, E_flux_func, grid.dv, axis=1)
-        
-        
-       
+        print('E_plus_matrix',E_flux.shape)
+        print('E_minus_matrix',E_flux.shape)
+        input()
         # HACKATHON: add collision terms and flux source terms here
 
         # You'll need to implement:
