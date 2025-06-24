97638a51f5578fcccbec214716ca5263d66929b0
pegahamiri <pamiri@cougarnet.uh.edu>
2025-06-24 16:19:31 -0700

    Update solver.py

--- src/spscml/straightforward_dlra/solver.py
+++ src/spscml/straightforward_dlra/solver.py
@@ -432,7 +432,7 @@ class Solver(eqx.Module):
 
         L_bcs = jnp.pad(L,[(0,0),(2,2)], mode='empty')
         E_flux_func = lambda left, right: E_plus_matrix @ left + E_minus_matrix @ right
-        E_flux = slope_limited_flux(L_bcs, SCHEME, E_flux_func, grid.dv, axis=1)
+        E_flux = slope_limited_flux_divergence(L_bcs, SCHEME, E_flux_func, grid.dv, axis=1)
         print('E_plus_matrix',E_plus_matrix.shape)
         print('E',E.shape)
         input()
